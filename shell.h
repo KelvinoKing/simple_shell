3371938d3108d3ab5ad238516a219e7d087fa16c
fardosa001 <fardosaadow@gmail.com>
2023-07-18 05:47:48 -0700

    Header file

--- shell.h
+++ shell.h
@@ -6,12 +6,19 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <dirent.h>
+#include <sys/stat.h>
 
 void callDir(void);
 int checkInput(char *str);
 int checkForPipes(char *str, char **argv);
 void execute_func(char **argcmd, char **argv);
 int _strcmp(char *s1, char *s2);
-
+char *_strdup(char *str);
+int _strncmp(char *s1, char *s2, size_t n);
+char *get_path(char *cmd);
+int _strlen(char *str);
+char *get_env(char *name);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+void print_env(char **env);
 #endif /*SHELL_H*/
