8368028fdb46d0cdabab78f7509ae1bdd36dac68
KelvinoKing <kelvinoking@outlook.com>
2023-07-19 08:59:37 +0000

    Returns lenght of string

---
+++ _strlen.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: function parameter
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+        int count = 0;
+
+        while (*(s + i) != '\0')
+        {
+                count++;
+                i++;
+        }
+
+        return (count);
+}
+
