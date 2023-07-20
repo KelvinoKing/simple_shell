86a593e76dfd7a4b9873dec4ce9c6190d8fc552d
KelvinoKing <kelvinoking@outlook.com>
2023-07-20 06:09:07 +0000

    Ensured no memory leaks

--- checkInput.c
+++ checkInput.c
@@ -1,27 +1,6 @@
 #include "shell.h"
 
 /**
- * _strcpy - copies string pointed to src to dest
- *
- * @dest: first argument
- * @src: second argument
- *
- * Return: char
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
-/**
  * checkInput - Accepts user input
  * @inputCmd: user command
  *
@@ -48,6 +27,7 @@ int checkInput(char *inputCmd)
 	if (buffer[0] != '\0')
 	{
 		_strcpy(inputCmd, buffer);
+		free(buffer);
 		return (0);
 	}
 	else
