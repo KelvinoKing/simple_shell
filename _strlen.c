f81a8a9c917955574fcbaa460c72b0c554db91c7
KelvinoKing <kelvinoking@outlook.com>
2023-07-20 06:08:28 +0000

    Modified _strlen function

--- _strlen.c
+++ _strlen.c
@@ -1,22 +1,21 @@
 #include "shell.h"
 /**
  * _strlen - Returns the length of a string
- *
  * @s: function parameter
  *
  * Return: int
  */
 int _strlen(char *s)
 {
-        int i = 0;
-        int count = 0;
+	int i = 0;
+	int count = 0;
 
-        while (*(s + i) != '\0')
-        {
-                count++;
-                i++;
-        }
+	while (*(s + i) != '\0')
+	{
+		count++;
+		i++;
+	}
 
-        return (count);
+	return (count);
 }
 
